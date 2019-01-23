83f4e822370255983e4f59693b3e71a0eb14637d
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-23 18:22:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,16 +7,4 @@ def square_array(array)
 end 
 
  
- =begin
- ([1, 2, 3]).each(*(array))
-
-new_array=[]
-  array.each{|num|new_array<<num**2}
-  new_array 
-  
-
-numbers = [1, 4, 9]
-  array.each do |num|
-    puts "#{array}"
-end
-=end
+ 
