a79cda89ff69fdf87d2b9414ba49dab1c7e5231f
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-23 18:32:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+   square_array = []
+  array.each do |num|
+    square_array << num * num
+  end
+  return square_array
+end 
+
+ 
+=begin
+my code 
+numbers = [1, 4, 9]
+  array.each do |num|
+    puts "#{array}"
 end
+=end
